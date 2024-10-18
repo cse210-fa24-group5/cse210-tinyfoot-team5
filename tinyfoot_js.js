9d9f30c0757ae13ec59873906cf73c1f40a0da04
JackLiX0105 <jacklix0105@gmail.com>
2024-10-18 12:37:34 -0700

    Starting point for tinyfoot

--- tinyfoot_js.js
+++ tinyfoot_js.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('.tinyfoot');
-    const popover = document.getElementById('popover');
+    const popover = document.createElement('div');
+    popover.id = 'popover';
+    document.body.appendChild(popover);
   
     buttons.forEach(button => {
+      button.textContent = '• • •';
       button.addEventListener('click', function (event) {
-        const footnoteText = event.target.getAttribute('data-footnote');
-        popover.textContent = footnoteText;
-  
-        const rect = event.target.getBoundingClientRect();
-        popover.style.top = `${rect.top + window.scrollY}px`;
+        popover.textContent = event.target.getAttribute('data-footnote');
+        const rect = button.getBoundingClientRect();
+        popover.style.top = `${rect.top}px`;
         popover.style.left = `${rect.right + 10}px`;
-  
         popover.style.display = 'block';
       });
     });
